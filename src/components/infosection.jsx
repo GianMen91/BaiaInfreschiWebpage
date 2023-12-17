f7f42d6941b2b4202535362cb2d448c1ce260ad6
GianMen91 <giancarlo.mennillo@gmail.com>
2023-12-17 22:50:58 +0100

    minor UI improvements and directory cleaning

--- src/components/infosection.jsx
+++ src/components/infosection.jsx
@@ -9,7 +9,7 @@ export const Infosection = ({ items }) => {
           <div key={index} className="info-container">
             <div className="row">
               <div className="col-xs-12 col-md-2">
-                <i className={`fa ${item.icon} icon`}></i>
+                <i className={`fa ${item.icon} icon`} style={{ color: '#6ec1e3' }}></i>
               </div>
               <div className="col-xs-12 col-md-10">
                 <div className="infosection-text">
