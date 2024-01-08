618fcfce67b51d084f4dad70b09805283ffc29da
GianMen91 <giancarlo.mennillo@gmail.com>
2024-01-08 09:55:28 +0100

    improve banner test and import jest in eslintrc.json file

--- src/components/banner/Banner.test.js
+++ src/components/banner/Banner.test.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { Banner } from './banner';
+import React from 'react'
+import { Banner } from './banner'
 
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() })
 
 describe('<Banner />', () => {
   it('renders the Banner component with correct title and text', () => {
     // Mock data
     const mockProps = {
       title: 'Test Title',
-      text: 'Test Text',
-    };
+      text: 'Test Text'
+    }
 
     // Render the component with mock props
-    const wrapper = shallow(<Banner {...mockProps} />);
+    const wrapper = Enzyme.shallow(<Banner {...mockProps} />)
 
     // Assertions
-    expect(wrapper.find('#banner').exists()).toBe(true);
-    expect(wrapper.find('.container').exists()).toBe(true);
-    expect(wrapper.find('.section-title').exists()).toBe(true);
-    expect(wrapper.find('h2').text()).toEqual('Test Title');
-    expect(wrapper.find('h3').text()).toEqual('Test Text');
-  });
-});
+    expect(wrapper.find('#banner').exists()).toBe(true)
+    expect(wrapper.find('.container').exists()).toBe(true)
+    expect(wrapper.find('.section-title').exists()).toBe(true)
+    expect(wrapper.find('h2').text()).toEqual('Test Title')
+    expect(wrapper.find('h3').text()).toEqual('Test Text')
+  })
+})
